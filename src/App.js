a0dd472e1ab605508f6f85ceb2091cb8059d4cef
Martyna <martyna.sowinska@hotmail.com>
2021-11-22 19:55:21 +0100

    Finished

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './App.css'
-import Home from './components/Home'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import MainSearch from './components/MainSearch'
+import CompanySearchResults from './components/CompanySearchResults'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-const App = () => (
-  <Home />
-)
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainSearch />} />
+        <Route path="/:companyName" element={<CompanySearchResults />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
 export default App
