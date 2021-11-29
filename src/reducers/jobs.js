834676c30794915c4f3df8cf9aae64f8c184146a
Martyna <martyna.sowinska@hotmail.com>
2021-11-29 17:00:36 +0100

    Finished reworking reducers

--- src/reducers/jobs.js
+++ src/reducers/jobs.js
@@ -2,13 +2,11 @@ import { initialState } from "../store";
 import { FETCH_JOBS } from '../actions/index'
 
 const jobsReducer = (state = initialState.jobs, action) => {
-  const { type, payload } = action;
-
-  switch (type) {
+  switch (action.type) {
     case FETCH_JOBS:
       return {
         ...state,
-        elements: payload,
+        elements: [...state.elements, action.payload],
       };
     default:
       return state;
